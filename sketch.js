863e6b375a15fd74b58c2758fb4f0d5a0f3bf339
radhikaghare <58420969+radhikaghare@users.noreply.github.com>
2020-05-28 19:52:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,21 +3,23 @@ var ring1,ring2,ring3;
 
 var tower1pressed;
 
+
+
 function setup() {
   createCanvas(500, 500);
- tower1 = createSprite(150,235,10,100);
+ tower1 = createSprite(110,205,10,200);
   tower1.shapeColor = "brown";
- tower2 = createSprite(200,235,10,100);
+ tower2 = createSprite(200,205,10,200);
   tower2.shapeColor = "brown";
- tower3 = createSprite(250,235,10,100);
+ tower3 = createSprite(300,205,10,200);
   tower3.shapeColor = "brown";
- base = createSprite(200,288,135,10);
+ base = createSprite(205,310,265,10);
   base.shapeColor = "brown";
- ring3 = createSprite(150,280,35,10);
+ ring3 = createSprite(110,300,75,10);
   ring3.shapeColor = "yellow";
- ring2 = createSprite(150,270,26,10);
+ ring2 = createSprite(110,290,55,10);
   ring2.shapeColor = "orange";
- ring1 = createSprite(150,260,20,10);
+ ring1 = createSprite(110,280,35,10);
   ring1.shapeColor = "red";
 
 }
@@ -25,39 +27,51 @@ function setup() {
 function draw() {
   background(220);
 
-  if(mousePressedOver(ring1) && ring1.x === 150){
-    ring1.y = 170; 
+  /*if(mousePresedOver(tower1)){
+    tower1pressed = true;
+  } else  {
+    tower1pressed = false;
   }
 
-if(mousePressedOver(tower2) /*&& ring1.y==240*/){
-  console.log("in tower2 Line 30 ring1.y " + ring1.y)
-  if(ring1.y===280) {
-    console.log("inside222")
-    ring1.y = 190;
-
+  if(mousePresedOver(tower2)){
+    tower2pressed = true;
+  } else{
+    tower2pressed = false;
   }
-  else {
-    console.log("inside ring1.y " + ring1.y)
+
+  if(mousePresedOver(tower3)){
+    tower3pressed = true;
+  }else {
+    tower3pressed = false;
+  }*/
+
+  console.log("RIng1 at start"+ ring1.x + ring1.y)
+  if(mousePressedOver(ring1) && ring1.x !=200){
+    ring1.y = 100; 
+  }
+
+  console.log("before tower pressed"+ ring1.x + ring1.y)
+  if (mousePressedOver(tower2) && ring1.y === 100 && ring1.x === 110){
     ring1.x = 200;
-    ring1.y = 280;
-    
-  
-  } 
-
-}
-if(mousePressedOver(tower3) ){
-  if(ring1.y==280) {
-  console.log("in tower3 Line 35")
-  ring1.x = 250;
-  ring1.y = 170;
-  } else {
-    ring1.x = 250;
-  ring1.y = 280;
+    ring1.y = 300;
   }
-}
-if(mousePressedOver(tower1) && ring1.y<169){
-  ring1.x = 150;
-  ring1.y = 280;
+
+  console.log("before ring pressed"+ ring1.x + ring1.y)
+  console.log("RIng1 at start"+ ring1.y + ring1.x)
+  if(mousePressedOver(ring1) && ring1.x === 200 && ring1.y ===300){
+    ring1.x = 200;
+    ring1.y = 90;
+  }
+
+
+/*if(mousePressedOver(tower1) && ring1.y<169){
+ if(ring1.y === 280){
+   ring1.y = 170;
+   ring1.x =150
+ } else{
+ring1.x = 150;
+
+ }
 }
 /*if(mousePressedOver(ring2) && ring2.x<ring1.x){
   ring2.y = 200;
@@ -93,3 +107,4 @@ if(mousePressedOver(tower1) && ring3.y==280){
 
   drawSprites();
 }
+
